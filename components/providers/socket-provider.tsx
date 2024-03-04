974adefa31bc2ae7409db8f61a1918976ab6cf34
lemidia <poiu2186@gmail.com>
2024-03-04 21:21:09 +0900

    codebase initiated & online user status added

--- components/providers/socket-provider.tsx
+++ components/providers/socket-provider.tsx
@@ -21,30 +21,12 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
   const [socket, setSocket] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
 
-  function detectDevice() {
-    if (
-      navigator.userAgent.match(/Android/i) ||
-      navigator.userAgent.match(/webOS/i) ||
-      navigator.userAgent.match(/iPhone/i) ||
-      navigator.userAgent.match(/iPad/i) ||
-      navigator.userAgent.match(/iPod/i) ||
-      navigator.userAgent.match(/BlackBerry/i) ||
-      navigator.userAgent.match(/Windows Phone/i)
-    ) {
-      return "phone";
-    } else {
-      return "computer";
-    }
-  }
-
   useEffect(() => {
     // Make socket instance for client.
     // It keep polling to the server.
     // Polling : client socket instance requests socket connection to the server repeatedly each every constant time intervals
     const socketInstance = new (ClientIO as any)(
-      detectDevice() === "phone"
-        ? "192.168.0.7:3000"
-        : process.env.NEXT_PUBLIC_SITE_URL!,
+      process.env.NEXT_PUBLIC_SITE_URL!,
       {
         path: "/api/socket/io",
         addTrailingSlash: false,
